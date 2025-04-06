bb04f809a1442bbd3c30c426f3417d1372900250
MaksLucky25 <maksrad93@gmail.com>
2025-04-06 17:08:02 +0300

    change testingRouter

--- src/routers/testingRouters.ts
+++ src/routers/testingRouters.ts
@@ -1,7 +1,5 @@
 import { Router } from 'express';
 import {blogCollection, commentCollection, postCollection, userCollection} from "../db/mongo-db";
-import {authService} from "../services/authService";
-
 
 
 export const testingRouters = Router();
@@ -14,21 +12,3 @@ testingRouters.delete('/all-data', async (req, res) => {
     res.sendStatus(204);
 });
 
-
-// Новые эндпоинты для тестов
-if (process.env.NODE_ENV === 'test') {
-    // Получить последний код подтверждения
-    testingRouters.get('/last-confirmation-code', async (req, res) => {
-        const user = await userCollection.findOne({});
-        res.status(200).json({ code: user?.emailConfirmation.confirmationCode });
-    });
-
-    // Подтвердить email без письма
-    testingRouters.post('/confirm-email', async (req, res) => {
-        const { code } = req.body;
-        const result = await authService.confirmRegistration(code);
-        result ? res.sendStatus(204) : res.sendStatus(400);
-    });
-
-
-}
